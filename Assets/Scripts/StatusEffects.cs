cef98fc9d5d5519adc7832b94510f85589cbe13b
ikeaorb <164377746+ikeaorb@users.noreply.github.com>
2026-08-08 00:12:08 -0500

    status effects unfinished

---
+++ Assets/Scripts/StatusEffects.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class StatusEffects : ScriptableObject
+{
+    public int Duration;
+    public int Damage;
+    public bool Stackable;
+    public int Stackability;
+}
