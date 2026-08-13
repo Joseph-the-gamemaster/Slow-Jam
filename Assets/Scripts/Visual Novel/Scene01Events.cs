b72d28da1a9953a162c13ddb2aa454265218d093
Joseph-the-gamemaster <josithgoodwin@gmail.com>
2026-08-12 20:23:58 -0500

    scrolling text

--- Assets/Scripts/Visual Novel/Scene01Events.cs
+++ Assets/Scripts/Visual Novel/Scene01Events.cs
@@ -11,7 +11,16 @@ public class Scene01Events : MonoBehaviour
     [SerializeField] AudioSource GirlSigh;
     [SerializeField] AudioSource GirlGasp;
 
-    
+        [SerializeField] string textToSpeak;
+        [SerializeField] int currentTextLength;
+        [SerializeField] int textLength;
+        [SerializeField] GameObject mainTextObject;
+
+        void Update()
+        {
+            textLength = TextCreator.charCount;
+        }
+
     void Start()
     {
         StartCoroutine(EventStarter());
@@ -24,8 +33,19 @@ public class Scene01Events : MonoBehaviour
         MC.SetActive(true);
         yield return new WaitForSeconds(2f);
         // this is where our text function will be called to display the text on the screen
-        TextBox.SetActive(true);
+        mainTextObject.SetActive(true);
+        textToSpeak = "Haruka's late? Well that's a surprise!";
+        TextBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
+        currentTextLength = textToSpeak.Length;
+        TextCreator.runTextPrint = true;
         GirlGasp.Play();
+        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(1f);
+        yield return new WaitUntil(() => textLength == currentTextLength);
+        yield return new WaitForSeconds(0.5f);
+
+
+        TextBox.SetActive(true);
         yield return new WaitForSeconds(2f);
         SideCharacter.SetActive(true);
         GirlSigh.Play();
--- Assets/Scripts/Visual Novel/TextCreator.cs
+++ Assets/Scripts/Visual Novel/TextCreator.cs
@@ -1,27 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TextCreator : MonoBehaviour
 {
-    public static TMPro.TMP_Text viewText;
+    public static TMP_Text viewText;
     public static bool runTextPrint;
     public static int charCount;
-    [SerializeField] string transferText;
-    [SerializeField] int internalCount;
 
-    // Update is called once per frame
+    [SerializeField] private TMP_Text myTextComponent; // Assign or grab automatically
+    [SerializeField] private string transferText;
+    [SerializeField] private int internalCount;
+
+    void Awake()
+    {
+        // Automatically get the component on this GameObject
+        myTextComponent = GetComponent<TMP_Text>();
+
+        // If viewText hasn't been set globally yet, default to this object's component
+        if (viewText == null)
+        {
+            viewText = myTextComponent;
+        }
+    }
+
     void Update()
     {
-        internalCount = charCount;
-        charCount = GetComponent<TMPro.TMP_Text>().text.Length;
-        if (runTextPrint == true)
+        // 1. Safety check: make sure the text component actually exists on this object
+        if (myTextComponent != null && myTextComponent.text != null)
+        {
+            internalCount = charCount;
+            charCount = myTextComponent.text.Length;
+        }
+
+        // 2. Run the typewriter effect when triggered
+        if (runTextPrint)
         {
             runTextPrint = false;
-            viewText.text = GetComponent<TMPro.TMP_Text>().text;
-            transferText = viewText.text;
-            viewText.text = "";
-            StartCoroutine(RollText());
+
+            if (viewText != null && myTextComponent != null)
+            {
+                transferText = myTextComponent.text;
+                viewText.text = "";
+                StartCoroutine(RollText());
+            }
+            else
+            {
+                Debug.LogError("TextCreator Error: 'viewText' or 'myTextComponent' is not assigned!");
+            }
         }
     }
 
@@ -29,7 +56,10 @@ public class TextCreator : MonoBehaviour
     {
         foreach (char c in transferText)
         {
-            viewText.text += c;
+            if (viewText != null)
+            {
+                viewText.text += c;
+            }
             yield return new WaitForSeconds(0.03f);
         }
     }
